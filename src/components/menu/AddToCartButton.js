76705057005d1c510a0a71752060a214bc680d9b
Lucas Barragana <lucasbarraga@gmail.com>
2024-07-28 16:26:44 -0300

    first commit

---
+++ src/components/menu/AddToCartButton.js
@@ -0,0 +1,24 @@
+
+export default function AddToCartButton({
+  hasSizesOrExtras, onClick, basePrice, image
+}) {
+  if (!hasSizesOrExtras) {
+    return (
+      <div className="flying-button-parent ">
+          <button className="bg-primary text-white rounded-full px-8 py-2 mt-4"
+          onClick={onClick}>
+            Add to cart ${basePrice}
+          </button>
+      </div>
+    );
+  }
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className="mt-4 bg-primary text-white rounded-full px-8 py-2"
+    >
+      <span>Add to cart (from ${basePrice})</span>
+    </button>
+  );
+}
